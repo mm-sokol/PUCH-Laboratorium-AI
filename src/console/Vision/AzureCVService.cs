b857c449a75b3914622b4f7f7265651096cf8425
Małgorzata Sokół <malgorzata@e-sokol.pl>
2024-12-12 17:44:50 +0100

    Added Custom Vision Service

---
+++ src/console/Vision/AzureCVService.cs
@@ -0,0 +1,13 @@
+// ReSharper disable all CheckNamespace
+
+namespace AzureCVision
+{
+    class AzureCVService
+    {
+        public static void Main(string[] args)
+        {
+            // your code here
+            Console.WriteLine("Hello world");
+        }
+    }
+}
